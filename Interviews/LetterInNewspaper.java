463b76ea650d8d4c628c0ba9426967b9801fd315
Sonal Raj <sonal.nitjsr@gmail.com>
2018-03-21 03:08:28 +0530

    Pramp mock interview question

--- Interviews/LetterInNewspaper.java
+++ Interviews/LetterInNewspaper.java
@@ -1,7 +1,8 @@
 /*
 You have written an anonymous love letter and you don’t want your handwriting to be recognized. 
 Since you don’t have a printer within reach, you are trying to write this letter by copying and pasting characters from a newspaper.
-Given a string L representing the letter and a string N representing the newspaper, return true if the L can be written entirely from N and false otherwise. 
+Given a string L representing the letter and a string N representing the newspaper, 
+return true if the L can be written entirely from N and false otherwise. 
 The letter includes only ascii characters.
 */
 
