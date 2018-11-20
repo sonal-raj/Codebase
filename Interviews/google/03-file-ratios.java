b5bd40578a18e96b4b71d90c66ce151ba005abde
Sonal Raj <sonal.nitjsr@gmail.com>
2018-11-21 01:12:13 +0530

    add google interview question

---
+++ Interviews/google/03-file-ratios.java
@@ -0,0 +1,13 @@
+/*
+ * You are given 2 files. 
+ * File 1 contains data in the format - 
+ *       A   B   0.5
+ *       B   C   0.1
+ *       ....
+ * File 2 contains entries in the following form - 
+ *       A   D
+ *       D   F
+ *       ...
+ *
+ * You need to calculate the ratio for every entry in file2. If not possible, result is -1.
+ */
