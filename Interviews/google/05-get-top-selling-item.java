4de58731f607e5cf8db23c6ee86d45791a955c84
Sonal Raj <sonal.nitjsr@gmail.com>
2018-11-21 01:21:38 +0530

    add google interview question

---
+++ Interviews/google/05-get-top-selling-item.java
@@ -0,0 +1,7 @@
+/*
+ * You are given data for a shopping website. The events are being
+ * received in the form of (pid, soldQuantity, timeStamp).
+ * You need to write a function for getting the top selling item 
+ * in the past one hour. 
+ */
+
