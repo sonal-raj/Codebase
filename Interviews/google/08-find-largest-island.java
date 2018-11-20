010b3597fbb927c4e5d25731d86ce88bc7b99dc5
Sonal Raj <sonal.nitjsr@gmail.com>
2018-11-21 01:33:01 +0530

    add google interview question

---
+++ Interviews/google/08-find-largest-island.java
@@ -0,0 +1,12 @@
+/*
+ * Let's say a land is represented by '1' and water is represented by '0'.
+ * You are given a matrix of 1's and 0's. You need to find the size of the largest island.
+ *
+ * For example:
+ *
+ * Input:  1 1 0 0 1
+ *         0 0 0 0 0
+ *
+ * Output: 2
+ *
+ */
