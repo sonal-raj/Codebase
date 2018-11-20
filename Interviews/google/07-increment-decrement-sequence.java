cb7039babccb1de6575aba9e8b78c4f423efdc42
Sonal Raj <sonal.nitjsr@gmail.com>
2018-11-21 01:29:19 +0530

    add google interview question

---
+++ Interviews/google/07-increment-decrement-sequence.java
@@ -0,0 +1,10 @@
+/*
+ * You are given a sequence of 'D' and 'I' signifying Decreasing and Increasing.
+ * Find the minimum possible sequence of numbers that can fulfill the D and I sequence.
+ *
+ * For example:
+ *
+ * Input:   D D I I D
+ * Output: 3 2 1 4 6 5
+ *
+ */
