885dc7f8dc026a2967c555c566c8960e022720ba
Sonal Raj <sonal.nitjsr@gmail.com>
2019-01-16 20:56:30 +0530

    Adding example script

---
+++ Interviews/WQ/minimum_start_value.py
@@ -0,0 +1,12 @@
+# In a game, you are given a sequence of integers, +ve signifying powerup and -ve specifying power spent in defeating a monster.
+# At no stage in the game should the power of the wizard go below 1. 
+#
+# Find the minimum start value of the power that the wizard should have, to successfully complete the game 
+#
+# Example 1
+# ---------
+#
+# Input : [ -2, 3, 1, -5 ]
+# Output : 4
+#
+
