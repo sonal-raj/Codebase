3785dd5852d98307ce536ffd6363ad300a5b29a5
Sonal Raj <sonal.nitjsr@gmail.com>
2019-03-16 18:58:25 +0530

    Add question

---
+++ Interviews/Jump/cutDownTrees.java
@@ -0,0 +1,14 @@
+/*
+ * There are N trees growing along a street. For safety reasons, one of the trees needs to be cut down. The local council wants the street to look ordered, so all the remaining trees should be sorted in non-decreasing order of height. Your goal is to count the number of ways this can be done. 
+ *
+ * Write a function:
+ *
+ * 	def solution(A):
+ *
+ * that, given an array A consisting of N integers, whgere A[K] denotes the height of the Kth tree. 
+ * returns the number of trees you can cut to satisfy the above condition. If the condition cannot ne satisfied, your function should return 0. 
+ *
+ * Example:
+ *
+ * A = [3, 4, 5, 4], your function should return 2. You can cut down the tree of height 5 or second tree of height 4.
+ */
